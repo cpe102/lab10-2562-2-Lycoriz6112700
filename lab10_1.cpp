b42f18cd75814d1472c020dfcad064f8f5ea3d9a
lab302-33 <lab302-33@labeng.local>
2020-01-31 17:30:51 +0700

    final

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,32 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int rollGrade;
+void roll(){
+    rollGrade = rand()%9 + 1;
+}
+
+int main(){
+    srand(time(0));
+    string Grade;
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    roll();
+    if (rollGrade == 1 )Grade = "A" ; 
+    else if (rollGrade == 2)Grade = "B+"; 
+    else if (rollGrade == 3)Grade = "B" ; 
+    else if (rollGrade == 4)Grade = "C+"; 
+    else if (rollGrade == 5)Grade = "C" ; 
+    else if (rollGrade == 6)Grade = "D+"; 
+    else if (rollGrade == 7)Grade = "D" ; 
+    else if (rollGrade == 8)Grade = "F" ; 
+    else if (rollGrade == 9)Grade = "W" ;
+
+    cout << "You will get "<< Grade <<" in this 261102.";
+
+    return 0;
+}
+
